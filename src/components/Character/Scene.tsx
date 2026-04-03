091dc4f9e2164559cbcf84cd77b4a57659b325d6
siddharthrastogi96 <siddharthrastogi221@gmail.com>
2026-04-03 10:54:25 -0400

    Adjust camera centering to balanced position

--- src/components/Character/Scene.tsx
+++ src/components/Character/Scene.tsx
@@ -82,7 +82,7 @@ const Scene = () => {
     camera.position.z = 10;
 
     // ⭐ CENTER FIX HERE
-    camera.position.set(9, 13.1, 24.7);
+    camera.position.set(4, 13.1, 24.7);
 
     camera.zoom = 1.1;
 
