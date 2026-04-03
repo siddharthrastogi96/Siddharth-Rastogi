e0084d789cca4381e105851804e4c08caa7c45ef
siddharthrastogi96 <siddharthrastogi221@gmail.com>
2026-04-03 13:45:01 -0400

    Character alignment desktop fix

--- src/components/Character/Scene.tsx
+++ src/components/Character/Scene.tsx
@@ -83,9 +83,9 @@ const Scene = () => {
 
       // Different base position for desktop vs mobile
       if (window.innerWidth > 1024) {
-        character.position.set(-2, 0.45, 0);
+        character.position.set(-3, 0.45, 0);
       } else {
-        character.position.set(0.1, 0.45, 0);
+        character.position.set(0.3, 0.45, 0);
       }
 
       scene.add(character);
